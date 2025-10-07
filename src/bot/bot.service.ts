a35638437b9de463a1a2207e5bc3a6eebacd3233
Azizbek Bariyev <azizbekbariyev35@gmail.com>
2025-10-07 17:09:32 +0500

    S

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -90,7 +90,7 @@ export class BotService {
               { id_telegram: userId },
               { token: token }
             );
-            const webAppUrl = `https://uz-milliy-test.uz/?token=${token}`;
+            const webAppUrl = `https://shamseducation.uz/?token=${token}`;
             //http://localhost:5173/?token=06cbc2e47cab18df94f0189bdef1f986767bd31ef47e01ffcad55e30559341b9
             await ctx.replyWithHTML(
               `Assalomu alaykum! 👋 ${ctx.from?.first_name}\n📋 Test ishlash uchun pastdagi tugmani bosing:`,
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function start() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     const allowedOrigins = [
-      "https://uz-milliy-test.uz"
+      "https://shamseducation.uz/"
     ];
 
     app.enableCors({
