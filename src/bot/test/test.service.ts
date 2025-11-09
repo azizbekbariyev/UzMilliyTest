9c524290eadd8dfee06838f603e0fb72b867d202
Azizbek Bariyev <azizbekbariyev35@gmail.com>
2025-11-10 00:28:16 +0500

    done

--- src/bot/test/test.service.ts
+++ src/bot/test/test.service.ts
@@ -44,7 +44,9 @@ export class TestService {
   }
 
   async endTest(ctx: Context) {
+    console.log(ctx.callbackQuery!["data"])
     const testId = ctx.callbackQuery!["data"].split("_")[2];
+    console.log(testId)
     const test = await this.testRepository.findOne({
       where: { test_id: testId },
     });
